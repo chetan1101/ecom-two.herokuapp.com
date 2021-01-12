187bb92b8ea2b38d2f6ccbdd1cdbbc8f9454aad5
chetan soni <chetan.soni@onsumaye.com>
2021-01-12 23:21:01 +0530

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv/config");
 const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
-const PORT  = process.env.PORT || 5000
+const PORT  = process.env.PORT
 const cors = require('cors');
 const bodyParser = require("body-parser");
-require("dotenv/config");
+
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
